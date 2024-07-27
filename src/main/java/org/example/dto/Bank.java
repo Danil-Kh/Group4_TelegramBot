31aeb8f8de6bc64e406d19a7e6c0992289d582c8
s1lent_games <s1lentgames.vanya9980@gmail.com>
2024-07-28 00:06:36 +0300

    full commit(s1lent's, Danya,s and  Irina,s projects(need addition of commits of other developers)) v. 0.4 preALPFA(fixed user.java completely + added full realisation of logic in currency+bank changing in DB by pressing the button) with notes.md to guide you through problems

--- src/main/java/org/example/dto/Bank.java
+++ src/main/java/org/example/dto/Bank.java
@@ -1,28 +1,19 @@
 package org.example.dto;
 
-
+import lombok.Getter;
 import java.net.URI;
 
-
+    @Getter
     public enum Bank {
-        NBU("NBU", URI.create("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json")),
-        PRIVATBANK("PrivatBank", URI.create("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5")),
-        MONOBANK("Monobank", URI.create("https://api.monobank.ua/bank/currency"));
+        NBU(URI.create("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json")),
+        PRIVATBANK(URI.create("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5")),
+        MONOBANK(URI.create("https://api.monobank.ua/bank/currency"));
 
-        private final String name;
         private final URI uri;
 
-        Bank(String name, URI uri) {
-            this.name = name;
+        Bank(URI uri) {
             this.uri = uri;
         }
 
-        public String getName() {
-            return name;
-        }
-
-        public URI getUri() {
-            return uri;
-        }
     }
 
