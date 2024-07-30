b94e75a67967915d4005d3d1230362633a8bf43a
s1lent_games <s1lentgames.vanya9980@gmail.com>
2024-07-30 22:06:13 +0300

    commit(prop testing))

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,14 +3,22 @@ package org.example;
 import org.example.telegrambot.TelegramBot;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
+import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.*;
 
 public class Test {
     static Logger logger = Logger.getLogger(Test.class.getName());
     public static void main(String[] args){
-
-        String botToken = "7138847779:AAHvBvcxW1CxC8CFGNCNeX9nXbtnX2Jq_Mo";
+        String botToken = "";
+        Properties prop = new Properties();
+        try {
+            prop.load(Test.class.getClassLoader().getResourceAsStream("config.properties"));
+            botToken = prop.getProperty("token");
+        }
+        catch (IOException ex) {
+            logger.log(Level.SEVERE, "IO Exception occurred", ex);
+        }
         try(TelegramBotsLongPollingApplication telegramBotsApi = new TelegramBotsLongPollingApplication()) {
             telegramBotsApi.registerBot(botToken, new TelegramBot(botToken));
             Thread.currentThread().join();
